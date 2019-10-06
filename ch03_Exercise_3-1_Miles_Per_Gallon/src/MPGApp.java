4950053ae4690a854e95e66528b6817272507fe5
Fred Wills <willsf422@gmail.com>
2019-10-05 21:10:14 -0400

    Miles per gallon

---
+++ ch03_Exercise_3-1_Miles_Per_Gallon/src/MPGApp.java
@@ -0,0 +1,37 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+
+public class MPGApp {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Miles Per Gallon calculator");
+        System.out.println();  // print a blank line
+        
+        @SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+        
+        String choice = "y";
+        while (choice.equalsIgnoreCase("y")) {
+            System.out.print("Enter miles driven: ");
+            double miles = sc.nextDouble();
+            
+            System.out.print("Enter gallons of gas used: ");
+            double gallons = sc.nextDouble();
+            
+            double mpg = miles/gallons;
+            DecimalFormat df = new DecimalFormat("#.##");      
+            mpg = Double.valueOf(df.format(mpg));
+            
+            System.out.println("Miles per gallon is: " + mpg + ".");
+            System.out.println(); 
+            
+            
+            
+            System.out.print("Calculate another MPG? (y/n): ");
+            choice = sc.next();
+            System.out.println();
+        }
+    }
+    
+}
