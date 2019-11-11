f2426a51ba6b493b41ba7208b1b5e1a438cfa2e2
Fred Wills <willsf422@gmail.com>
2019-11-10 22:27:09 -0500

    prs server project

---
+++ Prs_Server_Project/src/main/java/com/maxtrain/bootcamp/prs/user/User.java
@@ -0,0 +1,86 @@
+package com.maxtrain.bootcamp.prs.user;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String userName;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String phone;
+	private String email;
+	private boolean isReviewer;
+	private boolean isAdmin;
+	@Column(name = "isActive")
+	private boolean active;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstname(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastname(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public boolean isReviewer() {
+		return isReviewer;
+	}
+	public void setReviewer(boolean isReviewer) {
+		this.isReviewer = isReviewer;
+	}
+	public boolean isAdmin() {
+		return isAdmin;
+	}
+	public void setAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+	public boolean isActive() {
+		return active;
+	}
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	public User() {}
+	
+}
