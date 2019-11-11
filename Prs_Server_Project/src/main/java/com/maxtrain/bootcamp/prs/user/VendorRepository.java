add0ac2c299e849179ef7a0adc821c87f4f2e32b
Fred Wills <willsf422@gmail.com>
2019-11-10 23:59:12 -0500

    deleted out dated prs server project

--- Prs_Server_Project/src/main/java/com/maxtrain/bootcamp/prs/user/VendorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.maxtrain.bootcamp.prs.user;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface VendorRepository extends CrudRepository<Vendor, Integer>{
-
-}
