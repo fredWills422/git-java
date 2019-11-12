d42d723f17e669bae5f1635507e1302e39f919a4
Fred Wills <willsf422@gmail.com>
2019-11-11 20:35:46 -0500

    deleted prs server project

--- Prs_Server_Project/src/main/java/com/maxtrain/bootcamp/prs/user/RequestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.maxtrain.bootcamp.prs.user;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface RequestRepository extends CrudRepository<Request, Integer>{
-
-}
