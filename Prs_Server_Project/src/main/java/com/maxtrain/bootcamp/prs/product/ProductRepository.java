90a2012928780ba4f7ddd10f6f4e360f602ed523
Fred Wills <willsf422@gmail.com>
2019-11-13 12:02:21 -0500

    deleted outdated prs server project

--- Prs_Server_Project/src/main/java/com/maxtrain/bootcamp/prs/product/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.maxtrain.bootcamp.prs.product;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<Product, Integer>{
-
-}
--- Prs_Server_Project/src/main/java/com/maxtrain/bootcamp/prs/vendor/VendorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.maxtrain.bootcamp.prs.vendor;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface VendorRepository extends CrudRepository<Vendor, Integer>{
-
-}
