8540eb0ba5f85fff2be8b1afebf5556c105107ae
Fred Wills <willsf422@gmail.com>
2019-11-16 11:52:09 -0500

    deleted prs server project

--- Prs_Server_Project/src/main/java/com/maxtrain/bootcamp/prs/product/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.maxtrain.bootcamp.prs.product;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<Product, Integer>{
-	Product findByIdAndVendorId (Integer id, Integer vendorId);
-}
