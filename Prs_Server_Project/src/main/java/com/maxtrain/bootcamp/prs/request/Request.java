ac0352e16ded1a7a0dbd7266b65fd830567a1342
Fred Wills <willsf422@gmail.com>
2019-11-19 21:12:54 -0500

    updated requestController

---
+++ Prs_Server_Project/src/main/java/com/maxtrain/bootcamp/prs/request/Request.java
@@ -0,0 +1,126 @@
+package com.maxtrain.bootcamp.prs.request;
+
+
+//import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.maxtrain.bootcamp.prs.user.User;
+
+@Entity
+public class Request {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	@Column(length=80, nullable=false)
+	private String description;
+	@Column(length=80, nullable=false)
+	private String justification;
+	@Column(length=80, nullable=false)
+	private LocalDateTime dateNeeded;
+	@Column(length=80, nullable=false)
+	private String deliveryMode;
+	@Column(length=80, nullable=false)
+	private String status;
+	@Column(length=10, nullable=false)
+	private double total;
+	@Column(length=80, nullable=false)
+	private Date submittedDate;
+	@Column(length=80, nullable=true)
+	private String reasonForRejection;
+	@ManyToOne(optional = false)
+	@JoinColumn(name="userId")
+	private User user;
+	
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Request() {}
+	
+	public Request(int id, User user, String description, String justification, LocalDateTime dateNeeded, String deliveryMode,
+			String status, double total, Date submittedDate, String reasonForRejection) {
+		super();
+		this.id = id;
+		this.user = user;
+		this.description = description;
+		this.justification = justification;
+		this.dateNeeded = dateNeeded;
+		this.deliveryMode = deliveryMode;
+		this.status = status;
+		this.total = total;
+		this.submittedDate = submittedDate;
+		this.reasonForRejection = reasonForRejection;
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getJustification() {
+		return justification;
+	}
+	public void setJustification(String justification) {
+		this.justification = justification;
+	}
+	public LocalDateTime getDateNeeded() {
+		return dateNeeded;
+	}
+	public void setDateNeeded(LocalDateTime dateNeeded) {
+		this.dateNeeded = dateNeeded;
+	}
+	public String getDeliveryMode() {
+		return deliveryMode;
+	}
+	public void setDeliveryMode(String deliveryMode) {
+		this.deliveryMode = deliveryMode;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public double getTotal() {
+		return total;
+	}
+	public void setTotal(double total) {
+		this.total = total;
+	}
+	public Date getSubmittedDate() {
+		return submittedDate;
+	}
+	public void setSubmittedDate(Date submittedDate) {
+		this.submittedDate = submittedDate;
+	}
+	public String getReasonForRejection() {
+		return reasonForRejection;
+	}
+	public void setReasonForRejection(String reasonForRejection) {
+		this.reasonForRejection = reasonForRejection;
+	}
+	
+
+}
