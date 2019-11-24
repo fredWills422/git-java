002f04cb355c690b46e608cca48727980f9d7a23
Fred Wills <willsf422@gmail.com>
2019-11-23 23:49:38 -0500

    recalculateTotal method added

---
+++ Prs_Server_Project/src/main/java/com/maxtrain/bootcamp/prs/lineitem/LineItem.java
@@ -0,0 +1,83 @@
+package com.maxtrain.bootcamp.prs.lineitem;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import com.maxtrain.bootcamp.prs.product.Product;
+import com.maxtrain.bootcamp.prs.request.Request;
+
+@Entity
+@Table(uniqueConstraints=@UniqueConstraint(
+		name = "UIDX_request_product", columnNames = {"requestId", "productId"}))
+public class LineItem {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	@ManyToOne(optional=false)
+	@JoinColumn(name="RequestId", nullable = false)
+	private Request request;
+	@ManyToOne(optional=false)
+	@JoinColumn(name="ProductId", nullable = false)
+	private Product product;
+	@Column
+	private int quantity;
+	
+	public LineItem() {}
+
+	public LineItem(int id, Request request, Product product, int quantity) {
+		super();
+		this.id = id;
+		this.request = request;
+		this.product = product;
+		this.quantity = quantity;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Request getRequest() {
+		return request;
+	}
+
+	public void setRequest(Request request) {
+		this.request = request;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	@Override
+	public String toString() {
+		return "LineItem [id=" + id + ", request=" + request + ", product=" + product + ", quantity=" + quantity
+				+ ", getId()=" + getId() + ", getRequest()=" + getRequest() + ", getProduct()=" + getProduct()
+				+ ", getQuantity()=" + getQuantity() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+				+ ", toString()=" + super.toString() + "]";
+	}
+
+}
